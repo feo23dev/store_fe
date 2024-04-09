72db733f37fc2abd65ad238b4db67e65e9574b77
efe okyar <f.efe.okyar@gmail.com>
2024-04-09 17:38:49 +0300

    homepage fixed

--- js/index.js
+++ js/index.js
@@ -3,8 +3,12 @@ import Products from "./class/Products.js";
 
 const logged_user = new User();
 const products = new Products();
+
+let hamburgerIcon = document.querySelector(".menu-icon");
+hamburgerIcon.addEventListener("click", menuToggle);
 let MenuItems = document.getElementById("Menu-items");
 MenuItems.style.maxHeight = "0px";
+
 function menuToggle() {
   if (MenuItems.style.maxHeight == "0px") {
     MenuItems.style.maxHeight = "200px";
@@ -12,16 +16,3 @@ function menuToggle() {
     MenuItems.style.maxHeight = "0px";
   }
 }
-
-console.log("I AM FROM INDEX.JS ", logged_user);
-console.log("Products? ", products);
-
-const productsLink = document.getElementById("products-link");
-
-console.log("I AM INDEX.JS");
-
-productsLink.addEventListener("click", (event) => {
-  //   event.preventDefault();
-  console.log("I am clicked");
-  products.renderProducts();
-});
