894e2064d5b808a729419bf8368fee3f4c4a1190
efe okyar <f.efe.okyar@gmail.com>
2024-04-09 14:03:31 +0300

    homepage>index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,16 @@ import Products from "./class/Products.js";
 
 const logged_user = new User();
 const products = new Products();
+let MenuItems = document.getElementById("Menu-items");
+MenuItems.style.maxHeight = "0px";
+function menuToggle() {
+  if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+  } else {
+    MenuItems.style.maxHeight = "0px";
+  }
+}
+
 console.log("I AM FROM INDEX.JS ", logged_user);
 console.log("Products? ", products);
 
